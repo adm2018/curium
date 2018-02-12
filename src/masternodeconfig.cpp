1e32ebc73888923f02733ce9ebb16848226af35d
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-02-12 16:27:11 -0700

    yo

---
+++ src/masternodeconfig.cpp
@@ -0,0 +1,34 @@
+#include "masternodeconfig.h"
+#include "util.h"
+
+CMasternodeConfig masternodeConfig;
+
+void CMasternodeConfig::add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex) {
+    CMasternodeEntry cme(alias, ip, privKey, txHash, outputIndex);
+    entries.push_back(cme);
+}
+
+bool CMasternodeConfig::read(boost::filesystem::path path, std::string& strErr) {
+    boost::filesystem::ifstream streamConfig(GetMasternodeConfigFile());
+    if (!streamConfig.good()) {
+        return true; // No masternode.conf file is OK
+    }
+
+    for(std::string line; std::getline(streamConfig, line); )
+    {
+        if(line.empty()) {
+            continue;
+        }
+        std::istringstream iss(line);
+        std::string alias, ip, privKey, txHash, outputIndex;
+        if (!(iss >> alias >> ip >> privKey >> txHash >> outputIndex)) {
+            strErr = "Could not parse masternode.conf line: " + line;
+            streamConfig.close();
+            return false;
+        }
+        add(alias, ip, privKey, txHash, outputIndex);
+    }
+
+    streamConfig.close();
+    return true;
+}
