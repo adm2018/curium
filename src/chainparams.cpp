778b1f18ec40c47a10fcccb09caa1e24f0f018d9
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-02-17 17:17:13 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,7 +65,7 @@ public:
         vSeeds.push_back(CDNSSeedData("curium", "45.32.214.107"));
         vSeeds.push_back(CDNSSeedData("curium2", "207.246.120.137"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of( 76);                    // Curium addresses start with 'X'
+        base58Prefixes[PUBKEY_ADDRESS] = list_of( 28);                    // Curium addresses start with 'C'
         base58Prefixes[SCRIPT_ADDRESS] = list_of( 16);                    // Curium script addresses start with '7'
         base58Prefixes[SECRET_KEY] =     list_of(204);                    // Curium private keys start with '7' or 'X'
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x02)(0xFE)(0x52)(0xF8); // Curium BIP32 pubkeys start with 'cruv'
