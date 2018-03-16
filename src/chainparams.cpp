238100b576274a7f9ca28a898fdea551f11bf73a
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-03-16 17:04:28 -0600

    forgot to add the cscript

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -135,7 +135,7 @@ public:
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 50 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("043384710fa689ad5023690c80f3addf8f13f8d45b8c8ab357c96ddae9fac46bd8996b10f4d4604fa08dce601aac4997abcedf92f1fabc21b179c45070ac7b03a9") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
