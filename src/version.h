7dc6216cbb5bbc4b44de05dfcb26db913cb09653
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-03-20 17:31:41 -0600

    added new version

--- src/version.h
+++ src/version.h
@@ -10,16 +10,16 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70103;
+static const int PROTOCOL_VERSION = 70104;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70103;
+static const int GETHEADERS_VERSION = 70104;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70077;
+static const int MIN_PEER_PROTO_VERSION = 70103;
 
 //! minimum peer version accepted by DarksendPool
 static const int MIN_POOL_PEER_PROTO_VERSION = 70103;
