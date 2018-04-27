ed464ab40b3275f4aa0f4b9fc789d4ca6038a4bd
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-04-27 15:50:07 -0600

    fixed more mac content

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -16,5 +16,5 @@ wallet_packages=bdb
 upnp_packages=miniupnpc
 
 ifneq ($(build_os),darwin)
-darwin_native_packages=native_cctools native_cruit native_libdmg-hfsplus
+darwin_native_packages=native_cctools native_cdrkit native_libdmg-hfsplus
 endif
