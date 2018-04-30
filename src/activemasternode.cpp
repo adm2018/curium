2b2fcf39d4a7db2d495d9f378b3797189f3f93d1
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-04-30 14:24:43 -0600

    added amounts for 10k

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -209,8 +209,8 @@ void CActiveMasternode::ManageStateInitial()
         return;
     }
 
-    if(pwalletMain->GetBalance() < 1000*COIN) {
-        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 1000 CURIUM\n", GetStateString());
+    if(pwalletMain->GetBalance() < 10000*COIN) {
+        LogPrintf("CActiveMasternode::ManageStateInitial -- %s: Wallet balance is < 10000 CURIUM\n", GetStateString());
         return;
     }
 
