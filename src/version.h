48de602379a446ed2b6a7439481d8408073d3af3
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-05-14 06:40:53 -0700

    updated some stuff

--- src/version.h
+++ src/version.h
@@ -10,7 +10,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70104;
+static const int PROTOCOL_VERSION = 70110;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -19,13 +19,13 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70104;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70103;
+static const int MIN_PEER_PROTO_VERSION = 70104;
 
 //! minimum peer version accepted by DarksendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 70103;
+static const int MIN_POOL_PEER_PROTO_VERSION = 70110;
 
 //! minimum peer version for masternode budgets
-static const int MIN_BUDGET_PEER_PROTO_VERSION = 70103;
+static const int MIN_BUDGET_PEER_PROTO_VERSION = 70110;
 
 //! minimum peer version for masternode winner broadcasts
 static const int MIN_MNW_PEER_PROTO_VERSION = 70103;
@@ -34,7 +34,7 @@ static const int MIN_MNW_PEER_PROTO_VERSION = 70103;
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
 static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70103;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70103;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70110;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
